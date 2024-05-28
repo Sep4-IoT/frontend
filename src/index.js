8c8c198569a658a83ac35af59facaf7e5c823bc4
Jakub Kuka <jakubkuka07@gmail.com>
2024-05-28 20:57:55 +0200

    Reinstalling dependencies + updating index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import Root from "./routes/Root"
-import HomePage from "./routes/HomePage"
-import GreenhousePage from "./routes/GreenhousePage"
-import HistoryPage from "./routes/HistoryPage"
-import { RouterProvider, createHashRouter } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./routes/Root";
+import HomePage from "./routes/HomePage";
+import GreenhousePage from "./routes/GreenhousePage";
+import HistoryPage from "./routes/HistoryPage";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 const router = createHashRouter([
   {
-      path: "/",
-      element: <Root />,
-      children: [
-          {
-              path: "/frontend",
-              element: <HomePage />,
-          },
-          {
-              path: "/greenhouse",
-              element: <GreenhousePage />,
-          },
-          {
-              path: "/history",
-              element: <HistoryPage />,
-          },
-      ],
+    path: "/frontend/",
+    element: <Root />,
+    children: [
+      {
+        path: "frontend", 
+        element: <HomePage />,
+      },
+      {
+        path: "greenhouse",
+        element: <GreenhousePage />,
+      },
+      {
+        path: "history",
+        element: <HistoryPage />,
+      },
+    ],
   },
-])
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
